1b82599e816e1f317c4350e1f24145b3547e0305
kotfind <kotfind@yandex.ru>
2022-03-12 22:04:13 +0300

    Fix random; Upd random test

--- Random.py
+++ Random.py
@@ -1,12 +1,7 @@
 from random import random
 import numpy as np
-from math import sin, cos, pi
 
 def rand_vec():
-    phi   = random() * pi * 2
-    tetha = random() * pi * 2
-    return np.array(
-            [ cos(tetha) * sin(phi)
-            , sin(tetha) * sin(phi)
-            , cos(phi)
-            ])
+    v = np.array([np.random.normal() for _ in range(3)])
+    v /= np.linalg.norm(v)
+    return v
--- testRandom.py
+++ testRandom.py
@@ -1,10 +1,4 @@
 from Random import rand_vec
-from Plot import plot3d
 import numpy as np
 
-toplot = []
-for i in range(1000):
-    v = rand_vec()
-    toplot.append([v, v * (1 + 2e-2)])
-
-plot3d(toplot)
+print(np.sum(np.array([rand_vec() for _ in range(int(1e5))]), axis=0))
